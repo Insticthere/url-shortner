2ab94c5d4cb525ade3643b89a6d139c7aa26af76
Insticthere <omgnitingupta@gmail.com>
2023-06-15 16:10:11 +0530

    ok

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import { nanoid } from 'nanoid'
-import connectMongo from "@/utils/connectMongo";
 
 const urlSchema = new mongoose.Schema({
   id: { type: String, default: () => nanoid(8), unique: true },
@@ -8,15 +7,13 @@ const urlSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 });
 
-const URL = mongoose.model("URL", urlSchema);
+export const URL = mongoose.models.URL || mongoose.model("URL", urlSchema);
 
 export default async function handler(req, res) {
   if (req.method !== "GET" || !req.query.url)
     return res.status(400).json({ error: "Please provide a URL or use the correct method." });
 
   const resurl = req.query.url;
-
-  await connectMongo();
 
   const id = nanoid(8);
 
